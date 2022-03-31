1b0cdc7c4931e6414d376858e76d0628f5ef8d0e
adiasksui <Adrian.Stritzinger@askui.com>
2022-03-31 20:39:21 +0200

    fix: example spec

--- examples/askui-nodejs-base-example/src/specs/example.spec.ts
+++ examples/askui-nodejs-base-example/src/specs/example.spec.ts
@@ -1,31 +1,40 @@
 import * as askui from '@vqa4gui/askui';
-import { TestStepResultDto } from '@vqa4gui/askui/dist/cjs/core/model/test-case-result-dto';
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 60 * 1000 * 60;
 
 describe('Jasmine demo with askui', () => {
   let newClient: askui.Client;
   const controluiServerUrl = process.env.CI_JOB_ID ? 'chrome-runner' : 'localhost';
-
   beforeAll(async () => {
-    newClient = new askui.Client(
-      `http://${controluiServerUrl}:6769`,
-      'https://controlui-api-dev.askui.com/upload',
-    );
+    await askui.startAskuiServer();
+
+    newClient = new askui.Client({
+      controlServerUrl: `http://${controluiServerUrl}:6769`,
+      controlYourUiApi: 'https://controlui-api-dev.askui.com',
+      annotationLevel: 'onFailure',
+    });
     await newClient.start();
   });
 
   it('Should click on text', async () => {
-    const result = await newClient.click().text().exec();
-    expect((result as unknown as TestStepResultDto).state).toBe('PASSED');
+    const result = await newClient
+      .click()
+      .text()
+      .exec();
+    expect(result.state).toBe('PASSED');
   });
 
   it('Should fail', async () => {
-    const result = await newClient.expect().text().withText('NO such text exits').exists()
+    const result = await newClient
+      .expect()
+      .text()
+      .withText('No such text exits')
+      .exists()
       .exec();
     expect(result.state).toBe('FAILED');
   });
+
   afterAll(async () => {
-    await newClient.close();
+    await newClient.stop();
   });
 });
