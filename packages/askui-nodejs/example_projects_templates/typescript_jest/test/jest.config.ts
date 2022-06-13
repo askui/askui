ad411b1e7ea194402355645d8d927b4ea636212e
maxi-askui <maximilian.wessendorf@askui.com>
2022-06-13 08:39:20 +0200

    fix(jest.config.ts): updated jest version and added jest config for fast execution (#AS-1061)

--- packages/askui-nodejs/example_projects_templates/typescript_jest/test/jest.config.ts
+++ packages/askui-nodejs/example_projects_templates/typescript_jest/test/jest.config.ts
@@ -4,6 +4,9 @@ const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFilesAfterEnv: ['./helper/jest.setup.ts'],
+  sandboxInjectedGlobals: [
+    'Math',
+  ],
 };
 
 // eslint-disable-next-line import/no-default-export
