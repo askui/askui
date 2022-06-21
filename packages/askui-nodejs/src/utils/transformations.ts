5b76d8fc701c1a5007711b7db313bc686c4631b1
Thinh <thinh.nguyen@askui.com>
2022-06-21 16:29:01 +0200

    perf(sharp for jimp): verify value as number (#AS-1073)

--- packages/askui-nodejs/src/utils/transformations.ts
+++ packages/askui-nodejs/src/utils/transformations.ts
@@ -45,16 +45,16 @@ export async function resizeBase64ImageWithSameRatio(
     const imageHeight = imageMetadata.height;
     const imageWidth = imageMetadata.width;
 
-    if (Math.max(imageHeight ?? NaN, imageWidth ?? NaN) <= maxEdge) {
+    if (Math.max(imageHeight as number, imageWidth as number) <= maxEdge) {
       return await Promise.resolve({ base64Image: base64ImageString, resizeRatio });
     }
 
-    const imageRatio = (imageHeight ?? NaN) / (imageWidth ?? NaN);
+    const imageRatio = (imageHeight as number) / (imageWidth as number);
     const isHeigthBiggerThanWidth = imageRatio > 1;
     const newHeigh = isHeigthBiggerThanWidth ? maxEdge : maxEdge * imageRatio;
     const newWidth = isHeigthBiggerThanWidth ? maxEdge / imageRatio : maxEdge;
     const newImage = orignalImage.resize(newWidth, newHeigh);
-    resizeRatio = (imageHeight ?? NaN) / newHeigh;
+    resizeRatio = (imageHeight as number) / newHeigh;
     const newImageBase64 = `data:image/png;base64,${(await newImage.toBuffer()).toString('base64')}`;
     return await Promise.resolve({ base64Image: newImageBase64, resizeRatio });
   } catch (error) {
