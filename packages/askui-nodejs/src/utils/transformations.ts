6b21c8197afd46a133ee3c0e3950e50fd9452d13
Thinh <thinh.nguyen@askui.com>
2022-06-23 10:11:50 +0200

    perf: change resize with sharp for numerical improvement (#AS-1073)

--- packages/askui-nodejs/src/utils/transformations.ts
+++ packages/askui-nodejs/src/utils/transformations.ts
@@ -4,12 +4,35 @@ import { logger } from '../lib';
 import { ImageResizingError, InvalidBase64Image } from './image-resize-errors';
 import { ResizedImage } from './resized-image-interface';
 
+async function transformToBase64String(sharpImage: sharp.Sharp): Promise<string> {
+  const bufferedImage = await sharpImage.toBuffer();
+  const bufferedImageString = bufferedImage.toString('base64');
+  const base64ImageString = `data:image/png;base64,${bufferedImageString}`;
+
+  return base64ImageString;
+}
+
+async function bufferBase64String(base64ImageString: string): Promise<Buffer> {
+  const cutBase64ImageString: string = base64ImageString.replace(/^data:image\/png;base64,/, '');
+  const bufferOfImage = Buffer.from(cutBase64ImageString, 'base64');
+  return bufferOfImage;
+}
+
+async function getLengths(originalImage: sharp.Sharp): Promise<number []> {
+  const imageMetadata = await originalImage.metadata();
+  const imageHeight = imageMetadata.height as number;
+  const imageWidth = imageMetadata.width as number;
+  const arrLengths = [imageWidth, imageHeight];
+
+  return arrLengths;
+}
+
 export async function toBase64Image(imagePath: string): Promise<string> {
   if (!(fs.existsSync(imagePath))) {
     throw new Error(`the image ${imagePath} does not exists!`);
   }
   const image = sharp(imagePath);
-  return `data:image/png;base64,${(await image.toBuffer()).toString('base64')}`;
+  return transformToBase64String(image);
 }
 
 export async function toBase64ImageIfNeeded(pngPathOrBase64Image: string): Promise<string> {
@@ -39,29 +62,28 @@ export async function resizeBase64ImageWithSameRatio(
     Promise.reject(new InvalidBase64Image('Invalid base64 image string'));
   }
   try {
-    let resizeRatio = 1;
-    const orignalImage = sharp(Buffer.from(base64ImageString.replace(/^data:image\/png;base64,/, ''), 'base64'));
-    const imageMetadata = await orignalImage.metadata();
-    const imageHeight = imageMetadata.height;
-    const imageWidth = imageMetadata.width;
+    const resizeRatio = 1;
+    const bufferedbase64Image = await bufferBase64String(base64ImageString);
+    const originalImage = sharp(bufferedbase64Image);
+    const HeightandWidth = await getLengths(originalImage);
+    const width = HeightandWidth[0] as number;
+    const height = HeightandWidth[1] as number;
 
-    if (Math.max(imageHeight as number, imageWidth as number) <= maxEdge) {
-      return await Promise.resolve({ base64Image: base64ImageString, resizeRatio });
+    if (Math.max(height, width) <= maxEdge) {
+      return { base64Image: base64ImageString, resizeRatio };
     }
 
-    const imageRatio = (imageHeight as number) / (imageWidth as number);
-    const isHeigthBiggerThanWidth = imageRatio > 1;
-    const newHeigh = isHeigthBiggerThanWidth ? maxEdge : maxEdge * imageRatio;
-    const newWidth = isHeigthBiggerThanWidth ? maxEdge / imageRatio : maxEdge;
+    const newImage = originalImage.resize({
+      width: width > height ? maxEdge : undefined,
+      height: height > width ? maxEdge : undefined,
+      fit: sharp.fit.contain,
+    });
+    const newWidth = (await newImage.metadata()).width as number;
 
-    const newHeighint = Number.parseInt(newHeigh.toString(), 10);
-    const newWidthint = Number.parseInt(newWidth.toString(), 10);
-
-    const newImage = orignalImage.resize(newWidthint, newHeighint);
-    resizeRatio = (imageHeight as number) / newHeigh;
-    const newImageBase64 = `data:image/png;base64,${(await newImage.toBuffer()).toString('base64')}`;
-
-    return await Promise.resolve({ base64Image: newImageBase64, resizeRatio });
+    return {
+      base64Image: await transformToBase64String(newImage),
+      resizeRatio: width / newWidth,
+    };
   } catch (error) {
     return Promise.reject(new ImageResizingError(`A Problem has occured during the resizeing of the image. Error: ${error}`));
   }
