2efbbc5a9e53185c3e0bdf3de8efe91bbf4bf6a1
Adrian Stritzinger <Adrian.Stritzinger@askui.com>
2022-06-23 14:58:13 +0200

    docs: format default options as code to reflect types (#AS-979)

--- packages/askui-nodejs/src/lib/ui-controller-args.ts
+++ packages/askui-nodejs/src/lib/ui-controller-args.ts
@@ -3,26 +3,26 @@ import { LogLevels } from '../shared/log-levels';
 /**
  * Configuration options for the askui UI Controller
  *
- * @param {number} display - Default: 0
+ * @param {number} display - Default: `0`
  * You can choose on which display you want to excecute all tests.
  * 0 is your main monitor.
  * If you want to use your second monitor you can change
  * the value to `1` (`2` for your third monitor etc.).
- * @param {string} binaryVersion - Default: latest
+ * @param {string} binaryVersion - Default: `'latest'`
  *  Choose the version of the askui UI Controller Server.
- * @param {number} port - Default: 6769
+ * @param {number} port - Default: `6769`
  * The port the askui UI Controller is running on.
- * @param {string} host - Default: 127.0.0.1`
+ * @param {string} host - Default: `'127.0.0.1'`
  * The host the askui UI Controller is running on.
- * @param {boolean} minimize - Default: true
+ * @param {boolean} minimize - Default: `true`
  * askui UI Controller will be started as minimized window.
- * @param {boolean} overWriteBinary - Default: false
+ * @param {boolean} overWriteBinary - Default: `false`
  * Download the provided Version of askui UI Controller.
  * If a version is already downloaded. This version will be overwritten
- * @param {LogLevels} logLevel - Default: debug
+ * @param {LogLevels} logLevel - Default: `'debug'`
  * You can use different types of Log Level.
  * Options: "fatal", "error", "warn", "info", "debug", "trace", "silent", "verbose".
- * @param {string} logFilePath - Default: <temp-dir>/askui/askui-server.log
+ * @param {string} logFilePath - Default: `'<temp-dir>/askui/askui-server.log'`
  * It is possible to specify a path for your log files.
  * Per default we create the askui-server.log file and askui folder in your temp folder.
  */
