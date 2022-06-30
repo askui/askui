0f5824cf2dfceccb154f1cc482ab70d7e0c3b293
Adrian Stritzinger <Adrian.Stritzinger@askui.com>
2022-06-30 09:36:57 +0200

    feat: dont communicate missing timestamp as an error (#AS-1070)

--- packages/askui-nodejs/src/utils/analytics/installation-timestamp-get-error.ts
+++ packages/askui-nodejs/src/utils/analytics/installation-timestamp-get-error.ts
@@ -1,5 +1,5 @@
 export class InstallationTimestampGetError extends Error {
   constructor(err: NodeJS.ErrnoException) {
-    super(`Installation timestamp does not exist. \n${err.message}`);
+    super(`Installation timestamp does not exist. Try reinstalling the lib to create it. \n${err.message}`);
   }
 }
