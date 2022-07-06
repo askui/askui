6297dfe57fac34d5f11083cd232201b9cfdaec11
mlikasam-askui <samir.mlika@askui.com>
2022-07-06 14:37:19 +0200

    feat: migrate from tenant to workspace model (#AS-1137)

--- packages/askui-nodejs/src/execution/read-environment-credentials.spec.ts
+++ packages/askui-nodejs/src/execution/read-environment-credentials.spec.ts
@@ -3,9 +3,9 @@ import { envCredentials } from './read-environment-credentials';
 describe('envCredentials()', () => {
   test('should read the credentials from the environment variables', () => {
     process.env['ASKUI_TOKEN'] = 'token';
-    process.env['ASKUI_TENANT'] = 'tenant';
+    process.env['ASKUI_WORKSPACE_ID'] = 'id123';
     process.env['ASKUI_EMAIL'] = 'name@tenant.com';
     const credentialsFromTheEnv = envCredentials();
-    expect(credentialsFromTheEnv).toStrictEqual({ tenant: 'tenant', email: 'name@tenant.com', token: 'token' });
+    expect(credentialsFromTheEnv).toStrictEqual({ workspaceId: 'id123', email: 'name@tenant.com', token: 'token' });
   });
 });
--- packages/askui-nodejs/src/execution/read-environment-credentials.ts
+++ packages/askui-nodejs/src/execution/read-environment-credentials.ts
@@ -3,12 +3,12 @@ import { CredentialArgs } from '../utils/http/credentials';
 
 export function envCredentials(): CredentialArgs | undefined {
   const envToken = process.env['ASKUI_TOKEN'];
-  const envTenant = process.env['ASKUI_TENANT'];
+  const envWorkspaceId = process.env['ASKUI_WORKSPACE_ID'];
   const envEmail = process.env['ASKUI_EMAIL'];
-  if (envToken && envTenant && envEmail) {
+  if (envToken && envWorkspaceId && envEmail) {
     logger.info('Credentials are used from ENV variables: ASKUI_TOKEN, ASKUI_TENANT and ASKUI_EMAIL');
     return {
-      tenant: envTenant,
+      workspaceId: envWorkspaceId,
       email: envEmail,
       token: envToken,
     };
