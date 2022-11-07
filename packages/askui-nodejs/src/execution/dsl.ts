1f1e3289ad313dbab58535d5b4a9e396e40402cb
mlikasam-askui <samir.mlika@askui.com>
2022-11-07 11:48:56 +0100

    docs: add in code docs for the get commands (#AS-1398)

--- packages/askui-nodejs/src/execution/dsl.ts
+++ packages/askui-nodejs/src/execution/dsl.ts
@@ -3721,7 +3721,21 @@ export abstract class Getter extends FluentCommand {
    * - `bndbox`: location of element described with coordinates of a bounding box
    * **Examples:**
    * ```typescript
-   * const icon = await aui.get().icon().withText('care left').exec();
+   * const text = await aui.get().text().withText('Sign').exec();
+   * console.log(text);
+   * // console output: [
+   * //  DetectedElement {
+   * //     name: 'TEXT',
+   * //     text: 'Sign In',
+   * //     colors: [ 'black', 'gray', 'gray' ],
+   * //     bndbox: BoundingBox {
+   * //     xmin: 1128.2720982142857,
+   * //     ymin: 160.21332310267857,
+   * //     xmax: 1178.8204241071428,
+   * //     ymax: 180.83512834821428
+   * //     }
+   * // }
+   * // ]
    * ```
    *
    *
@@ -3742,7 +3756,32 @@ export abstract class Getter extends FluentCommand {
    * - `bndbox`: location of element described with coordinates of a bounding box
    * **Examples:**
    * ```typescript
-   * const detectedElements = await aui.getAll().exec()
+   * const detectedElements = await aui.getAll().exec();
+   * console.log(detectedElements);
+   * // console output: [
+   * //  DetectedElement {
+   * //     name: 'TEXT',
+   * //     text: 'Sign In',
+   * //     colors: [ 'black', 'gray', 'gray' ],
+   * //     bndbox: BoundingBox {
+   * //     xmin: 1128.2720982142857,
+   * //     ymin: 160.21332310267857,
+   * //     xmax: 1178.8204241071428,
+   * //     ymax: 180.83512834821428
+   * //     },
+   * //  DetectedElement {
+   * //     name: 'ICON',
+   * //     text: 'search',
+   * //     colors: [ 'black', 'red', 'gray' ],
+   * //     bndbox: BoundingBox {
+   * //     xmin: 250.8204241071428,
+   * //     ymin: 300.21332310267857,
+   * //     xmax: 450.6304241071428,
+   * //     ymax: 950.47812834821428
+   * //     },
+   * //     ... 381 more items
+   * // }
+   * // ]
    * ```
    *
    * @return {ExecGetter}
