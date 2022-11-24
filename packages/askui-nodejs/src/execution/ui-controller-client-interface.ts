eab1c0dc441a221ed5a8f73f787d9f2588054745
mlikasam-askui <samir.mlika@askui.com>
2022-11-24 21:23:23 +0100

    feat add client side resize flag

--- packages/askui-nodejs/src/execution/ui-controller-client-interface.ts
+++ packages/askui-nodejs/src/execution/ui-controller-client-interface.ts
@@ -7,6 +7,12 @@ import { ProxyAgentArgs } from '../shared/proxy-agent-args';
  *
  * @param {string} uiControllerUrl - Default: http://127.0.0.1:6769
  * The adress of the askui UI Controller server.
+ * @param {boolean} resize - Default: undefined. Resizing will be skipped.
+ * Side length of the target image to resize to in px.
+ * Your screen will be resized with the original size ratio based on this number,
+ *  which will be equal to the lenghst image side.
+ * This is helpful in case of bad internet conection to reduce the request size,
+ *  but it can cause a decrease in the prediction quality
  * @param {string} inferenceClientUrl - Default: https://inference.askui.com`
  * Address of the askui Inference server.
  * @param {AnnotationLevel} annotationLevel - Default: AnnotationLevel.DISABLED
@@ -24,6 +30,7 @@ export interface ClientArgs {
   readonly annotationLevel?: AnnotationLevel,
   readonly credentials?: CredentialArgs,
   readonly proxyAgents?: ProxyAgentArgs
+  readonly resize?: number
 }
 
 export interface ClientArgsWithDefaults extends ClientArgs {
