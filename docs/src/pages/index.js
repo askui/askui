15bae86b08419b7e4a9f4960b5e65fb30d09e498
JohannesDienst-askui <johannes.dienst@askui.com>
2023-01-17 12:13:57 +0100

    docs: remove unused code and files in index.js; Use svg-file for github logo

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -1,28 +1,4 @@
 import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.scss';
-import HomepageFeatures from '../components/HomepageFeatures';
-
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.tagline}</h1>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/general/Introduction/why-askui">
-            Getting Started
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
 
 import {Redirect} from '@docusaurus/router';
 export default function Home() {
