495db9b9cece4f9df4aedae2152c7d1b976f4cbe
JohannesDienst-askui <johannes.dienst@askui.com>
2023-02-01 12:06:24 +0100

    docs: port next docs to versioned_docs/0.7.2. Change docusaurus config and index.js accordingly

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 
 import {Redirect} from '@docusaurus/router';
 export default function Home() {
-    return <Redirect to="/docs/general/Getting Started/getting-started" />;
+    return <Redirect to="/docs/general/Getting Started/start" />;
 }
