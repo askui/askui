ae691e69ff0b41ce74e639c9f12f5824107ce1a2
JohannesDienst-askui <johannes.dienst@askui.com>
2023-02-24 08:59:13 +0100

    docs: only add trackLink() when the Try Out Gitpod Card is visible

--- docs/static/scripts/segmentExternalLinks.js
+++ docs/static/scripts/segmentExternalLinks.js
@@ -1,6 +1,10 @@
 if (typeof analytics !== 'undefined' && typeof analytics.trackLink !== 'undefined') {
     var cardPicture = document.getElementById('github-try-out-image');
     var cardButton = document.getElementById('github-try-out-button');
-    analytics.trackLink(cardPicture, 'Clicked Gitpod Try Out Repository', {});
-    analytics.trackLink(cardButton, 'Clicked Gitpod Try Out Repository', {});
+    if (typeof cardPicture !== 'undefined') {
+        analytics.trackLink(cardPicture, 'Clicked Gitpod Try Out Repository', {});
+    }
+    if (typeof cardButton !== 'undefined') {
+        analytics.trackLink(cardButton, 'Clicked Gitpod Try Out Repository', {});
+    }
 }
