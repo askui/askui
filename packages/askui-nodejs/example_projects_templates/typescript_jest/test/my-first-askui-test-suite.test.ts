28d7ed9d4597036af3dbde1f91b062e4c60aa2b0
JohannesDienst-askui <johannes.dienst@askui.com>
2023-04-14 14:29:57 +0200

    docs: make the exec() in example project not as easily misable

--- packages/askui-nodejs/example_projects_templates/typescript_jest/test/my-first-askui-test-suite.test.ts
+++ packages/askui-nodejs/example_projects_templates/typescript_jest/test/my-first-askui-test-suite.test.ts
@@ -1,14 +1,14 @@
 import { aui } from './helper/jest.setup';
 
 describe('jest with askui', () => {
-
   it('should generate an interactive annotation', async () => {
     await aui.annotateInteractively();
   });
 
   it('should click on my element', async () => {
     await aui
-      .click()
-      // <INSERT YOUR COPIED FILTER HERE AND UNCOMMENT THIS LINE>.exec();
+      .click();
+    // <INSERT YOUR COPIED FILTER HERE AND UNCOMMENT THIS AND THE NEXT LINE>
+    // .exec();
   });
 });
