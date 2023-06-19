1deb0269ad7c285b4f2c1fde2acc293a3cffe966
JohannesDienst-askui <johannes.dienst@askui.com>
2023-06-19 15:31:56 +0200

    docs: merge commit

---
+++ docs/.alexrc.js
@@ -0,0 +1,15 @@
+// See https://github.com/words/cuss/blob/master/index.json what is flagged
+exports.profanitySureness = 1;
+
+exports.allow = [
+    "basically",
+    "easy",
+    "gal-guy",
+    "host-hostess",
+    "invalid",
+    "just",
+    "of-course",
+    "simple",
+    "special",
+    "whitespaces",
+];
