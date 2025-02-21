6dd565d0a4eef43acbb9603a395449d05468a104
Dominik Klotz <105296959+programminx-askui@users.noreply.github.com>
2025-02-21 11:21:24 +0100

    Update ai-elements-args.ts

--- packages/askui-nodejs/src/core/ai-element/ai-elements-args.ts
+++ packages/askui-nodejs/src/core/ai-element/ai-elements-args.ts
@@ -8,9 +8,9 @@
  *
  * @property {'Warn' | 'Error' | 'Ignore'} onLocationNotExist - Action to take when
  *  one or more specified directories do not exist.
- *      - `'Warn'` → Logs a warning if a directory is missing.
- *      - `'Error'` → Throws an error if a directory is missing.
- *      - `'Ignore'` → Skips missing directories without logging anything.
+ *      - `'warn'` → Logs a warning if a directory is missing.
+ *      - `'error'` → Throws an error if a directory is missing.
+ *      - `'igone'` → Skips missing directories without logging anything.
  *  This action is triggered when specified directories do not exist,
  *      not when AI elements within those directories are missing.
  */
